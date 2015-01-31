b9de5de937ce7b820927e709356b38c8a372b1dc
Andee tao <andeetao@andees-mac-pro.attlocal.net>
2015-01-31 15:22:53 -0800

    fix sqling

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <body>
 <h1>A small example page to insert some data in to the MySQL database using PHP</h1>
 <form action="insert.php" method="post">
-Firstname: <input type="text" name="fname" /><br><br>
-Lastname: <input type="text" name="lname" /><br><br>
+Firstname: <input type="text" name="firstname" /><br><br>
+Lastname: <input type="text" name="lastname" /><br><br>
  
 <input type="submit" />
 </form>
